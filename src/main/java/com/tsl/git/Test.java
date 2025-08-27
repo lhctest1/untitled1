a8b8a795019863ff21170673a9009597c42ed70d
lhctest1 <lhctest@mtx52.store>
2025-08-27 18:41:34 +0800

    Update Test.java

--- src/main/java/com/tsl/git/Test.java
+++ src/main/java/com/tsl/git/Test.java
@@ -4,7 +4,7 @@ import com.sun.xml.internal.ws.addressing.WsaActionUtil;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("dawdaw");
+        System.out.println("嘻嘻嘻~");
         System.out.println("test456");
         System.out.println("test789");
         System.out.println("test101112");
